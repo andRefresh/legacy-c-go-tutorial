9e741ff8db57a3dc5fba4a2e8dbde968adb61513
Andrea Freschi <freschiandrea86@gmail.com>
2024-08-04 22:45:55 +0200

    Fixed indent

--- part1/shm/shm.c
+++ part1/shm/shm.c
@@ -5,26 +5,27 @@ struct Shm *Shm;
 struct Shm *OpenShm(char *shmName)
 {
 
-        HANDLE fileHandle = CreateFile(shmName,
-                                       GENERIC_WRITE | GENERIC_READ,
-                                       FILE_SHARE_READ | FILE_SHARE_WRITE,
-                                       NULL,
-                                       CREATE_NEW,
-                                       FILE_ATTRIBUTE_TEMPORARY,
-                                       NULL);
+    HANDLE fileHandle = CreateFile(shmName,
+                                   GENERIC_WRITE | GENERIC_READ,
+                                   FILE_SHARE_READ | FILE_SHARE_WRITE,
+                                   NULL,
+                                   CREATE_NEW,
+                                   FILE_ATTRIBUTE_TEMPORARY,
+                                   NULL);
 
-        if (GetLastError() == ERROR_FILE_EXISTS) {
-            fileHandle = CreateFile(shmName,
-                                       GENERIC_WRITE | GENERIC_READ,
-                                       FILE_SHARE_READ | FILE_SHARE_WRITE,
-                                       NULL,
-                                       OPEN_EXISTING,
-                                       FILE_ATTRIBUTE_TEMPORARY,
-                                       NULL);
-        }
+    if (GetLastError() == ERROR_FILE_EXISTS)
+    {
+        fileHandle = CreateFile(shmName,
+                                GENERIC_WRITE | GENERIC_READ,
+                                FILE_SHARE_READ | FILE_SHARE_WRITE,
+                                NULL,
+                                OPEN_EXISTING,
+                                FILE_ATTRIBUTE_TEMPORARY,
+                                NULL);
+    }
 
-        HANDLE shmFileMapping = CreateFileMapping(fileHandle, NULL, PAGE_READWRITE, 0, sizeof(struct Shm), NULL);
-        Shm = (struct Shm *)MapViewOfFile(shmFileMapping, FILE_MAP_ALL_ACCESS, 0, 0, 0);
+    HANDLE shmFileMapping = CreateFileMapping(fileHandle, NULL, PAGE_READWRITE, 0, sizeof(struct Shm), NULL);
+    Shm = (struct Shm *)MapViewOfFile(shmFileMapping, FILE_MAP_ALL_ACCESS, 0, 0, 0);
 
     return Shm;
 }
